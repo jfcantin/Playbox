d1f31a6dc5bc010879731b7633039b460ee25473
Jean-Francois Cantin <jfcantin@ajmpc.com>
2010-03-10 10:19:07 -0700

    Initial Load

---
+++ product/SampleProject/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SampleProject
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ product/SampleProject/SampleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+
+namespace SampleProject
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [RowTest]
+        [Row(1,1,2)]
+        [Row(1,2,3)]
+        [Row(1,3,4)]
+        [Row(2,2,4)]
+        public void AddsTwoNumberTogether(int firstNumber, int secondNumber, int expectedResult)
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Add(firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
